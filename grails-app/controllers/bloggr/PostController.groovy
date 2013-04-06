3b6fc9735da28c37e4ad09272904169834719924
Jeff Bonhag <bonhag@vsac.org>
2013-04-06 00:07:26 -0400

    Properly map Groovy Date to Ember Date

--- grails-app/controllers/bloggr/PostController.groovy
+++ grails-app/controllers/bloggr/PostController.groovy
@@ -8,7 +8,19 @@ class PostController {
     def list() {
         // if only life could be this simple...
         // render Post.list() as JSON
-        def data = [ posts: Post.list() ]
-        render data as JSON
+
+        def results = Post.list()
+        render(contentType: "application/json") {
+            posts = array {
+                for (p in results) {
+                    post id: p.id,
+                         title: p.title,
+                         author: p.author,
+                         intro: p.author,
+                         extended: p.extended,
+                         published_at: p.publishedAt.format("MM/dd/yyyy")
+                }
+            }
+        }
     }
 }
