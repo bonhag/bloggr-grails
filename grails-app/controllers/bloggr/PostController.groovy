e898e2870f665d9812874a4714da402c9c0c96f1
Jeff Bonhag <bonhag@vsac.org>
2013-04-06 18:22:24 -0400

    Clean up JSON rendering for list()

--- grails-app/controllers/bloggr/PostController.groovy
+++ grails-app/controllers/bloggr/PostController.groovy
@@ -5,22 +5,28 @@ import grails.converters.JSON
 class PostController {
 
     def index() { }
+
     def list() {
         // if only life could be this simple...
         // render Post.list() as JSON
 
-        def results = Post.list()
-        render(contentType: "application/json") {
-            posts = array {
-                for (p in results) {
-                    post id: p.id,
-                         title: p.title,
-                         author: p.author,
-                         intro: p.author,
-                         extended: p.extended,
-                         published_at: p.publishedAt.format("MM/dd/yyyy")
-                }
+        def data = [
+            posts: Post.list().collect { p ->
+                [
+                    id: p.id,
+                    title: p.title,
+                    author: p.author,
+                    intro: p.intro,
+                    extended: p.extended,
+                    published_at: p.publishedAt.format("MM/dd/yyyy")
+                ]
             }
-        }
+        ]
+
+        render data as JSON
+    }
+
+    def show() {
+        render Post.get(params.id) as JSON
     }
 }
